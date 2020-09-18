6544c74f5e40d6c149242bf002164d09b15f8135
vhk.forwork@gmail.com <vhk.forwork@gmail.com>
2020-09-18 16:45:35 +0700

    fix ip - port heroku

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/', function(req, res) {
 
 //Sever IP - Port
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "127.0.0.1");
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP);
 
 var server = http.createServer(app);
 server.listen(app.get('port'), app.get('ip'), function() {
