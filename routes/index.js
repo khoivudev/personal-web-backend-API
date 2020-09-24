b30d372ceca97feb4b41cde1115886f410ad8deb
vhk.forwork@gmail.com <vhk.forwork@gmail.com>
2020-09-24 10:54:23 +0700

    Call API IP lookup

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
     res.render('pages/index', { title: 'Home | K-Zone' });
 });
 
+/*Client info */
+router.get('/client-info', (req, res) => {
+    res.render('pages/client_info', { title: 'My info | K-Zone' });
+})
+
 module.exports = router;
